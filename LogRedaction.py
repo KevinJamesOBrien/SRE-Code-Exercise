2ed110277779de472419cb2e760b7c13924f77e6
Kevin O'Brien <j.kevin.obrien@gmail.com>
2022-11-24 23:56:29 -0500

    Moved some files and started work on LogRedaction.py

--- LogRedaction.py
+++ LogRedaction.py
@@ -3,5 +3,9 @@ import os
 Comments added here between three "s
 https://www.geeksforgeeks.org/how-to-iterate-over-files-in-directory-using-python/
 """
-directory="SampleFiles"
+directory = 'SampleFiles'
+
+for filename in os.scandir(directory):
+    print(filename.name)
+
 print("Hello World")
