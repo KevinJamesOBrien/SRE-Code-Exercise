640e74a50079846c9cf43a7f90392c286c014c20
Kevin O'Brien <j.kevin.obrien@gmail.com>
2022-11-24 23:54:00 -0500

    Moved some files and started work on LogRedaction.py

--- LogRedaction.py
+++ LogRedaction.py
@@ -1,7 +1,7 @@
 import os
-
 """
 Comments added here between three "s
+https://www.geeksforgeeks.org/how-to-iterate-over-files-in-directory-using-python/
 """
-
+directory="SampleFiles"
 print("Hello World")
